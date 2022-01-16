4b38198de02bf09ba60840e2a14286112c7df56b
Moran <45389409+Moruanz@users.noreply.github.com>
2022-01-16 22:42:36 +0800

    Create BinaryTest.java
    The concept of binary, decimal, octal and hexadecimal

---
+++ Java Part 1/BinaryTest.java
@@ -0,0 +1,18 @@
+// The concept of binary, decimal, octal and hexadecimal 
+
+public class BinaryTest {
+	//insert main
+	public static void main(String[] args){
+    
+    //Represent n1 in binary
+	int n1 = 0b1010; //Binary start with 0b or 0B
+	int n2 = 1010; //This is decimal
+	int n3 = 01010; //This is octal, start with 0
+	int n4 = 0X10101; //This is hexadecimal, start with 0X
+
+	System.out.println("n1=" + n1);
+	System.out.println("n2=" + n2);
+	System.out.println("n3=" + n3);
+	System.out.println("n4=" + n4);
+    }
+}

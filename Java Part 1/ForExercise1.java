1ec1ae75ecbfb5596f4425ca63fc5ca202825c52
Moran <45389409+Moruanz@users.noreply.github.com>
2022-01-17 23:03:20 +0800

    Update ForExercise1.java

--- Java Part 1/ForExercise1.java
+++ Java Part 1/ForExercise1.java
@@ -14,16 +14,19 @@ public class ForExercise1 {
 
 
 
-  int count = 0; //variable to store the count 
-	int sum = 0; 
-	for (int i = 1 ; i <= 100; i++ ) {//first, output number 1-100
-		if( i % 9 == 0 ){
-			count++;
-			sum += i;
-			System.out.println("i = " + i);
+    int count = 0; //variable to store the count 
+	int sum = 0; //variable to store the sum
+	int start = 1;//separate variable for start number
+	int end = 100;//separate variable for end number
+	for (int i = start ; i <= end; i++ ) {//first, output number 1-100
+	if( i % 9 == 0 ){
+		count++;
+		sum += i;
+		System.out.println("i = " + i);
 		}
     }
     System.out.println("There are " + count +" such number");
     System.out.println("The sum is " + sum);
 	}
 }
+

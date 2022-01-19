5b4dc0cc3697397067656aaaa9a79eff35a3a9de
Moran <45389409+Moruanz@users.noreply.github.com>
2022-01-19 23:31:47 +0800

    Update nineNineTable.java
    To obtain 9 equations for the last line. Instead of 9 equations at 1st line

--- Java Part 1/nineNineTable.java
+++ Java Part 1/nineNineTable.java
@@ -5,7 +5,7 @@ public class nineNineTable {
 
 	int k = 1;
 	for (int i = 1; i <= 9; i++) {
-		for (int j = i; j <= 9; j++){
+		for (int j = 1; j <= i; j++){
 			System.out.print( i + " * " + j + " = " + (i*j) + "\t");
 		}
 		System.out.print("\n");
